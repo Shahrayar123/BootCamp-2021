ad648ebbd23ccb58eb380d8003ee7117af8b4826
Shahrayar <shahrayarmalik86@gmail.com>
2021-07-09 23:26:56 +0500

    Todo app in typescript

--- TypeScript/Todo_App/dist/index.js
+++ TypeScript/Todo_App/dist/index.js
@@ -1,2 +1,23 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const todo_1 = require("./todo");
 console.clear();
-console.log("Hello World");
+let todo_list = [];
+// creating object of Todo class
+let task1 = new todo_1.Todo(1, "Go for walk");
+let task2 = new todo_1.Todo(2, "Attend Meeting", true);
+let task3 = new todo_1.Todo(3, "Eat baryani");
+let task4 = new todo_1.Todo(4, "Work on Project", true);
+let task5 = new todo_1.Todo(5, "Attend party");
+// adding tasks in list
+todo_list.push(task1);
+todo_list.push(task2);
+todo_list.push(task3);
+todo_list.push(task4);
+todo_list.push(task5);
+// display tasks infpormation
+task1.displayTask();
+task2.displayTask();
+task3.displayTask();
+task4.displayTask();
+task5.displayTask();
--- TypeScript/Todo_App/src/index.ts
+++ TypeScript/Todo_App/src/index.ts
@@ -1,2 +1,41 @@
+import {Todo} from './todo';
+
 console.clear();
-console.log("Hello World");
+
+
+
+let todo_list: Todo[] = [];
+
+// creating object of Todo class
+let task1: Todo = new Todo(1, "Go for walk");
+let task2: Todo = new Todo(2, "Attend Meeting", true);
+let task3: Todo = new Todo(3, "Eat baryani");
+let task4: Todo = new Todo(4, "Work on Project", true);
+let task5: Todo = new Todo(5, "Attend party");
+
+// adding tasks in list
+todo_list.push(task1);
+todo_list.push(task2);
+todo_list.push(task3);
+todo_list.push(task4);
+todo_list.push(task5);
+
+
+// display tasks infpormation
+
+task1.displayTask();
+
+task2.displayTask();
+
+task3.displayTask();
+
+task4.displayTask();
+
+task5.displayTask();
+
+
+
+
+
+
+
