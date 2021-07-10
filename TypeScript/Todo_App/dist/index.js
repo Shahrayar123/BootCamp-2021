fb1ea06ccdf8a6a2a4c745490ce0a818a8df636b
Shahrayar <shahrayarmalik86@gmail.com>
2021-07-10 21:09:04 +0500

    todo console app

--- TypeScript/Todo_App/dist/index.js
+++ TypeScript/Todo_App/dist/index.js
@@ -7,7 +7,7 @@ let todo_list = [];
 let task1 = new todo_1.Todo(1, "Go for walk");
 let task2 = new todo_1.Todo(2, "Attend Meeting", true);
 let task3 = new todo_1.Todo(3, "Eat baryani");
-let task4 = new todo_1.Todo(4, "Work on Project", true);
+let task4 = new todo_1.Todo(4, "Work on Project");
 let task5 = new todo_1.Todo(5, "Attend party");
 // adding tasks in list
 todo_list.push(task1);
@@ -15,9 +15,39 @@ todo_list.push(task2);
 todo_list.push(task3);
 todo_list.push(task4);
 todo_list.push(task5);
-// display tasks infpormation
+console.log();
+console.log("\n----------> INITIAL LIST\n");
+// display tasks information
 task1.displayTask();
 task2.displayTask();
 task3.displayTask();
 task4.displayTask();
 task5.displayTask();
+console.log("\n----------> CHANGE STATUS OF TASK 1 & TASK 4\n");
+// change status of tasks
+task1.changeTaskStatus();
+task4.changeTaskStatus();
+console.log("\n----------> LIST AFTER CHANGING STATUS OF TASK 1 & 4\n");
+// display tasks information
+task1.displayTask();
+task2.displayTask();
+task3.displayTask();
+task4.displayTask();
+task5.displayTask();
+console.log("\n----------> DELETING A TASKS WHICH IS COMPLATED\n");
+// deleting the tasks which is completed
+for (let index = 0; index < todo_list.length; index++) {
+    if (todo_list[index]['taskStatus'] == true) {
+        // todo_list.splice(index, 1);
+        // delete todo_list[index];
+        console.log(todo_list[index]['taskStatus']);
+    }
+}
+console.log("\n----------> DISPLAYING LIST AFTER DELETING TASKS\n");
+// display tasks information
+task1.displayTask();
+task2.displayTask();
+task3.displayTask();
+task4.displayTask();
+task5.displayTask();
+// console.log(todo_list[0]['taskStatus']);
--- TypeScript/Todo_App/src/index.ts
+++ TypeScript/Todo_App/src/index.ts
@@ -3,14 +3,13 @@ import {Todo} from './todo';
 console.clear();
 
 
-
 let todo_list: Todo[] = [];
 
 // creating object of Todo class
 let task1: Todo = new Todo(1, "Go for walk");
 let task2: Todo = new Todo(2, "Attend Meeting", true);
 let task3: Todo = new Todo(3, "Eat baryani");
-let task4: Todo = new Todo(4, "Work on Project", true);
+let task4: Todo = new Todo(4, "Work on Project");
 let task5: Todo = new Todo(5, "Attend party");
 
 // adding tasks in list
@@ -20,8 +19,10 @@ todo_list.push(task3);
 todo_list.push(task4);
 todo_list.push(task5);
 
+console.log();
 
-// display tasks infpormation
+console.log("\n----------> INITIAL LIST\n");
+// display tasks information
 
 task1.displayTask();
 
@@ -34,8 +35,50 @@ task4.displayTask();
 task5.displayTask();
 
 
+console.log("\n----------> CHANGE STATUS OF TASK 1 & TASK 4\n");
+
+// change status of tasks
+task1.changeTaskStatus();
+task4.changeTaskStatus();
+
+console.log("\n----------> LIST AFTER CHANGING STATUS OF TASK 1 & 4\n");
+// display tasks information
+
+task1.displayTask();
+
+task2.displayTask();
+
+task3.displayTask();
+
+task4.displayTask();
+
+task5.displayTask();
 
 
+console.log("\n----------> DELETING A TASKS WHICH IS COMPLATED\n");
+
+// deleting the tasks which is completed
+
+// for(let index = 0; index < todo_list.length; index++)
+// {
+//     if(todo_list[index]['taskStatus'] == true)
+//     {
+//         todo_list.splice(index, 1);                
+//     }
+// }
+
+// console.log("\n----------> DISPLAYING LIST AFTER DELETING TASKS\n");
+
+// // display tasks information
+// task1.displayTask();
+
+// task2.displayTask();
+
+// task3.displayTask();
+
+// task4.displayTask();
+
+// task5.displayTask();
 
 
 
